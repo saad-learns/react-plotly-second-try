1290ad900cc7fbdf1c36e1d63c787c34e7662451
Mashrur Mia (Sa'ad) <mashrur.mia@gmail.com>
2022-07-29 11:47:42 +0100

    Display Bar chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Plot from 'react-plotly.js';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Plot
+      data={[
+          {
+          x: [1, 2, 3],
+          y: [2, 5, 3],
+          type: 'bar',
+        },
+      ]}
+      layout={{width: 600, height: 600, title: 'A Fancy Plot'}}
+    />
   );
-}
+};
 
 export default App;
